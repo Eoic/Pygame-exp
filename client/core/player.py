9a40355cc606c85a05930211f4b685dc3837bf0e
Eoic <karolis.strazdas@protonmail.com>
2023-11-13 22:25:52 +0200

    Simplify player implementation.

--- client/core/player.py
+++ client/core/player.py
@@ -1,59 +1,58 @@
 import uuid
 import pygame.transform
-from enum import Enum
-from typing import Tuple
+from pygame import key, K_w, K_s, K_a, K_d
+from core.constants import PIXELS_PER_UNIT
 from core.world_object import WorldObject
-from pygame import Vector2, key, K_w, K_s, K_a, K_d, sprite
-from core.math.interpolator import Interpolator
 
 
 class Player(WorldObject):
     id: uuid.UUID
     speed: float
+    direction: pygame.Vector2
 
-    def __init__(
-        self,
-        group: sprite.Group,
-        speed: float = 0.0,
-        position: Vector2 = Vector2(0, 0)
-    ) -> None:
-        super().__init__(image_path='assets/player.png', group=group, scale=0.10)
+    def __init__(self, size: float, color: pygame.Color, speed: float = 0.0) -> None:
+        super().__init__(color=color)
         self.id = uuid.uuid4()
         self.speed = speed
         self.acc_position = self.transform.position
+        self.rect.size = [size, size]
+        self.direction = pygame.Vector2(0, 0)
 
     def handle_input(self):
         keys = key.get_pressed()
-        direction = Vector2(0, 0)
 
-        if keys[K_w]:
-            direction.y = -1
-        if keys[K_s]:
-            direction.y = 1
-        if keys[K_a]:
-            direction.x = -1
-        if keys[K_d]:
-            direction.x = 1
+        if keys[K_w] and self.direction.y != 1:
+            self.direction.x = 0
+            self.direction.y = -1
+        elif keys[K_s] and self.direction.y != -1:
+            self.direction.x = 0
+            self.direction.y = 1
 
-        self.transform.set_direction(direction)
+        if keys[K_a] and self.direction.x != 1:
+            self.direction.x = -1
+            self.direction.y = 0
+        elif keys[K_d] and self.direction.x != -1:
+            self.direction.x = 1
+            self.direction.y = 0
+
+        self.transform.set_direction(self.direction)
 
     def handle_update(self, delta_time: float):
         if self.transform.direction.length() != 0:
-            base = 50
-            new_position = self.acc_position + self.speed * delta_time * self.transform.direction.normalize()
-            self.acc_position = new_position
-            new_position = pygame.Vector2(base * round(new_position.x / base), base * round(new_position.y / base))
-            self.transform.position = new_position
-            self.rect.center = self.transform.position
+            self.acc_position = self.acc_position + self.speed * delta_time * self.transform.direction.normalize()
+            self.transform.position = pygame.Vector2(
+                PIXELS_PER_UNIT * round(self.acc_position.x / PIXELS_PER_UNIT),
+                PIXELS_PER_UNIT * round(self.acc_position.y / PIXELS_PER_UNIT)
+            )
+
+    def handle_render(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
 
     def set_speed(self, value: float) -> None:
         if value < 0:
             raise ValueError('Speed cannot be negative.')
 
         self.speed = value
-
-    def set_color(self, color: Tuple[int, int, int]):
-        self.color = color
 
     def __str__(self) -> str:
         return f'Player(id={self.id})'
--- client/game.py
+++ client/game.py
@@ -1,9 +1,14 @@
 import pygame
 from core.events import EventHandler
-from core.camera import CameraGroup
-from core.world_object import WorldObject
 from core.player import Player
-from core.math.interpolator import Interpolator
+
+
+# Examples
+# Text:
+# font = pygame.freetype.SysFont('Cascadia Mono', 16)
+# self.surface.fill((42, 42, 42))
+# text_surface_one, _ = font.render(f'Player: {player.transform.position}', (255, 255, 255))
+# self.surface.blit(text_surface_one, (40, 40))
 
 
 class GameId:
@@ -16,8 +21,7 @@ class Game(GameId):
     clock: pygame.time.Clock
     surface: pygame.Surface
     target_fps: int
-    camera: CameraGroup
-    resolution = (1280, 720)
+    resolution = (1920, 1080)
 
     def __init__(self, target_fps: int = 60):
         pygame.init()
@@ -29,44 +33,21 @@ class Game(GameId):
         self.surface = pygame.display.set_mode(self.resolution, pygame.RESIZABLE)
 
     def run(self):
-        font = pygame.freetype.SysFont('Cascadia Mono', 16)
-
-        self.camera = CameraGroup()
-        tree = WorldObject(scale=0.4, image_path='assets/tree.png', group=self.camera)
-        tree.transform.set_position(pygame.Vector2(0, 0))
-
-        player = Player(speed=440.0, group=self.camera)
-        player.transform.set_position(pygame.Vector2(self.surface.get_width() / 2 - 20, self.surface.get_height() / 2 - 20))
-        self.camera.set_target(pygame.Vector2(self.resolution[0] // 2, self.resolution[1] // 2))
         self.is_running = True
-
-        interpolator = Interpolator((0, 0), self.resolution, 5, self.target_fps, shape=1.2)
+        player = Player(speed=220.0, size=50, color=pygame.Color(64, 255, 64))
+        player.transform.set_position(pygame.Vector2(self.surface.get_width() / 2, self.surface.get_height() / 2))
 
         while self.is_running:
             for event in pygame.event.get():
                 EventHandler.notify(event)
 
             self.surface.fill((42, 42, 42))
-            text_surface_one, _ = font.render(f'Player: {player.transform.position}', (255, 255, 255))
-            text_surface_two, _ = font.render(f'Camera offset: {self.camera.offset}', (255, 255, 255))
-            self.surface.blit(text_surface_one, (40, 40))
-            self.surface.blit(text_surface_two, (40, 80))
-
             player.handle_input()
             player.handle_update(self.delta_time)
-
-            # pygame.draw.circle(self.surface, pygame.Color(255, 128, 64), interpolator.pos, 25.0)
-            # interpolator.next()
-
-            self.camera.update()
-            self.camera.custom_draw()
+            player.handle_render(self.surface)
 
             pygame.display.update()
             self.delta_time = self.clock.tick(self.target_fps) / 1000
-
-    @EventHandler.register(GameId, pygame.VIDEORESIZE)
-    def on_resize(self, _event):
-        self.camera.set_half_size(self.surface.get_size()[0] // 2, self.surface.get_size()[1] // 2)
 
     @EventHandler.register(GameId, pygame.QUIT)
     def on_exit(self, _event):
