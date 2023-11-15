e39550b153ada6946b1f1db26016bd890f820093
indeform <devops@indeform.com>
2023-11-15 23:38:17 +0200

    Do not spawn food on player cells.

--- client/core/food_spawner.py
+++ client/core/food_spawner.py
@@ -6,11 +6,17 @@ from core.constants import WORLD_WIDTH, WORLD_HEIGHT, CELL_SIZE
 class FoodSpawner:
     food_position: pygame.Vector2
 
-    def spawn_food(self):
-        self.food_position = pygame.Vector2(
-            CELL_SIZE * round(random.randint(CELL_SIZE, WORLD_WIDTH - CELL_SIZE) / CELL_SIZE),
-            CELL_SIZE * round(random.randint(CELL_SIZE, WORLD_HEIGHT - CELL_SIZE) / CELL_SIZE)
-        )
+    def spawn_food(self, player):
+        ignored_cells = [player.transform.position] + player.trail
 
-    def consume_food(self):
-        self.spawn_food()
+        while True:
+            current_x = CELL_SIZE * round(random.randint(CELL_SIZE, WORLD_WIDTH - CELL_SIZE) / CELL_SIZE)
+            current_y = CELL_SIZE * round(random.randint(CELL_SIZE, WORLD_HEIGHT - CELL_SIZE) / CELL_SIZE)
+
+            for position in ignored_cells:
+                if current_x == position.x and current_y == position.y:
+                    continue
+            
+            break
+
+        self.food_position = pygame.Vector2(current_x, current_y)
